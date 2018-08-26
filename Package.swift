79c729dc68d7f44781857657747702653496dfce
Johan Segerfeldt <johan.segerfeldt@me.com>
2018-08-26 08:26:21 +0200

    Add Libpq C library module maps

--- Package.swift
+++ Package.swift
@@ -19,8 +19,15 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "PostgreSQL",
+            name: "LibpqMac",
             dependencies: []),
+        .target(
+            name: "LibpqLinux",
+            dependencies: []),
+
+        .target(
+            name: "PostgreSQL",
+            dependencies: ["LibpqMac", "LibpqLinux"]),
         .testTarget(
             name: "PostgreSQLTests",
             dependencies: ["PostgreSQL"]),
